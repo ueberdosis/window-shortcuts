ec5d1d3eb4841d74cdc1cff6636b9ffeef6c8a57
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-29 22:41:23 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-module.exports = () => {
-	if (process.platform === 'darwin') {
-		return require('./lib').sync();
-	}
+const path = require('path')
+const { execFileSync } = require('child_process')
+const bin = path.join(__dirname, './main')
 
-	throw new Error('sorry, macOS only.');
-};
+const parseMac = stdout => {
+	try {
+		const result = JSON.parse(stdout)
+		if (result !== null) {
+			result.platform = 'macos'
+			return result
+		}
+	} catch (error) {
+		throw new Error('Error parsing window data')
+	}
+}
+
+module.exports = () => parseMac(execFileSync(bin, { encoding: 'utf8' }))
+
